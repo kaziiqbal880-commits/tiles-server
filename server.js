f1a8ae2b80162e2f6362ff0fb8a79b7d381f4b29
kaziiqbal880 <kaziiqbal880@gmail.com>
2026-08-25 13:47:08 -0700

    Fix JSON server deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 5000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, "0.0.0.0", () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
